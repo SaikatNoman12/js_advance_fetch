bb001b65bd92785ded9e6209f7cd9d2592798afa
saikatnoman12 <noommaa240@gmail.com>
2022-10-24 01:05:07 +0600

    add js fetch

---
+++ README.md
@@ -0,0 +1,11 @@
+# JavaScript fetch data
+
+## Fetch method:
+
+1. GET
+
+2. POST
+
+3. PUT
+
+4. DELETE
--- app.js
+++ app.js
@@ -35,3 +35,141 @@ function showSecondData(data) {
     });
 }
 
+
+
+// fetch data in  my json file:---------
+fetch('./json/data.json')
+    .then(res => res.json())
+    .then(data => showThirdData(data))
+    .catch(err => {
+        console.log(err);
+    });
+
+function showThirdData(data) {
+    const demo3 = document.querySelector('.demo3')
+
+    data.forEach(ele => {
+
+        const h3 = document.createElement('h3');
+
+        h3.innerHTML = `${ele.name} - ${ele.city} - ${ele.religion} <br>`;
+
+        demo3.appendChild(h3);
+
+    });
+
+}
+
+
+
+// Post data
+fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'Foo',
+        body: 'Bar',
+        userId: '1'
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+    }
+})
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch((err) => {
+        console.log('Your error is: ', err);
+    });
+
+/* Output:-
+{
+    body: "Bar"
+    id: 101
+    title: "Foo"
+    userId: "1"
+}
+*/
+
+
+// Use form and submit:
+
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById('title');
+    const body = document.getElementById('body');
+    const userId = document.getElementById('id');
+
+    const value = {
+        title: title.value,
+        body: body.value,
+        userId: userId.value
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(
+            // {
+            //     title: title.value,
+            //     body: body.value,
+            //     userId: userId.value
+            // }
+            value
+        ),
+        headers: {
+            'Content-type': 'application/json; charset= UTF-8'
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch((err) => {
+            console.log('Your error is: ', err);
+        });
+
+});
+
+
+// another way post data:------------
+document.getElementById('form2').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const forms = document.forms[1];
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: new FormData(forms),
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch((err) => {
+            console.log('Your error is: ', err);
+        });
+
+});
+
+
+// Put data:---------
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PUT',
+    body: JSON.stringify({
+        id: 1,
+        title: 'FOO',
+        body: 'BAR',
+        userId: 1
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    }
+})
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => {
+        console.log('Your error is:', err);
+    });
+
+// delete data :------------
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'DELETE'
+});
